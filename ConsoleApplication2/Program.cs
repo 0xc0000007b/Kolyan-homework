3fbdd9422f3a5e6140d1c8484ca23a54bef2938e
0xc000007b <rammleid123@gmail.com>
2023-01-17 18:00:56 +0300

    check-sem-2

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Xml;
 using ConsoleApplication2.Seminars;
 
 namespace ConsoleApplication2
@@ -9,20 +13,68 @@ namespace ConsoleApplication2
         private static SeminarOne _semOne = new SeminarOne();
         public static void Main(string[] args)
         {
+           
+            
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
-            Console.Write("input your number ");
-            var num = int.Parse(Console.ReadLine());
-            Console.Write("input your second number ");
-            var numOne = int.Parse(Console.ReadLine());
-            for (int i = 1; i < num; i++)
+          
+            int number = int.Parse(Console.ReadLine());
+
+            var count = GetValue(number);
+
+            Console.WriteLine($"Количество необходимых действий над числом - {count}");       
+        }
+
+        private static int GetValue(int number)
+        {
+            var count = 0;
+
+            if (number == 1) return 0;
+            else
             {
-                Console.Write($" {i}");
+                count = 1;
+                count += number % 2 == 0 ? GetValue(number >> 1) : GetValue(number * 3 + 1);
             }
-            _semOne.FindMax(num, numOne); // first task
-            _semOne.FindMaxFromThree(num, numOne); // second Task
+
+            return count;
+        }
+        // double sum = 0;
+            // for(int i = 0; i > startValue && i < endValue; i++)
+            // {
+            //
+            //     sum += (double)i;
+            // }
+            // Enumerable
+            //
+            // var answer = Math.Ceiling(sum / 4.66981132075472);
+            //
+            //
+            // Console.WriteLine($"Сумма простых чисел = {answer}");
+        }
+
+        // private static string FindDuplicates( string num, string numOne)
+        // {
+        //     var res = string.Empty;
+        //     var num_arr = num.ToArray();
+        //     num_arr.Intersect(numOne.ToArray()).ToList().ForEach(number =>
+        //     {
+        //         Enumerable.Range(0, num_arr.Count(n => n == number)).Select(n => number).ToList().ForEach(n => res += n + " ");
+        //         res += "\r\n";
+        //     });
+        //     return res;
+        // }
+
+        // Console.Write("input your number ");
+            // var num = int.Parse(Console.ReadLine());
+            // Console.Write("input your second number ");
+            // var numOne = int.Parse(Console.ReadLine());
+            // for (int i = 1; i < num; i++)
+            // {
+            //     Console.Write($" {i}");
+            // }
+            // _semOne.FindMax(num, numOne); // first task
+            // _semOne.FindMaxFromThree(num, numOne); // second Task
         }
 
         
-    }
-}
+    
---
+++ ConsoleApplication2/Seminars/SeminarTwo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApplication2.Seminars
+{
+    public class SeminarTwo
+    {
+        public static void FindSecond()
+        {
+            int n = int.Parse(Console.ReadLine());
+            n /= 10;
+            n %= 10;
+            Console.WriteLine(n);
+        } 
+        public static void FindThird()
+        {
+            int n = int.Parse(Console.ReadLine());
+            while (n > 999) n /= 10;
+            if (n > 100) n %= 10;
+                
+            Console.WriteLine(n);
+            
+        }
+
+        public static void CheckWeekDay()
+        {
+            int day;
+            switch (day = int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    Console.WriteLine("monday");
+                    break;
+                case 2:
+                    Console.WriteLine("tuesday");
+                    break;
+                
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("No day");
+                    break;
+            }
+        }
+    }
+}
